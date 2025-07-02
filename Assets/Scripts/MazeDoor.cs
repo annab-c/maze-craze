85eb3eddd8ddd6f3f4baef42f07adc34198099f3
annab-c <annabalazscsiki@gmail.com>
2025-07-03 00:08:56 +0200

    Post-Processing added to achieve a retro look ✨

--- Assets/Scripts/MazeDoor.cs
+++ Assets/Scripts/MazeDoor.cs
@@ -1,22 +1,55 @@
 using UnityEngine;
 
-public class MazeDoor : MazePassage {
+public class MazeDoor : MazePassage
+{
+    private MazeDoor OtherSideOfDoor
+    {
+        get
+        {
+            return otherCell.GetEdge(direction.GetOpposite()) as MazeDoor;
+        }
+    }
 
-		private MazeDoor OtherSideOfDoor {
-		get {
-			return otherCell.GetEdge(direction.GetOpposite()) as MazeDoor;
-		}
-	}
-	
-	public override void Initialize (MazeCell primary, MazeCell other, MazeDirection direction) {
-		base.Initialize(primary, other, direction);
-		if (OtherSideOfDoor != null) {
-			hinge.localScale = new Vector3(-1f, 1f, 1f);
-			Vector3 p = hinge.localPosition;
-			p.x = -p.x;
-			hinge.localPosition = p;
-		}
-	}
-	
-	public Transform hinge;
+    public Transform hinge;
+    public float openAngle = 90f;
+    public float speed = 2f;
+    private bool opening = false;
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+
+    void Start()
+    {
+        closedRotation = hinge.localRotation;
+        openRotation = Quaternion.Euler(0f, openAngle, 0f) * closedRotation;
+    }
+
+    void Update()
+    {
+        // Simple example: press E to toggle (could be replaced with proximity checks later)
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            opening = !opening;
+        }
+
+        if (opening)
+        {
+            hinge.localRotation = Quaternion.Lerp(hinge.localRotation, openRotation, Time.deltaTime * speed);
+        }
+        else
+        {
+            hinge.localRotation = Quaternion.Lerp(hinge.localRotation, closedRotation, Time.deltaTime * speed);
+        }
+    }
+
+    public override void Initialize(MazeCell primary, MazeCell other, MazeDirection direction)
+    {
+        base.Initialize(primary, other, direction);
+        if (OtherSideOfDoor != null)
+        {
+            hinge.localScale = new Vector3(-1f, 1f, 1f);
+            Vector3 p = hinge.localPosition;
+            p.x = -p.x;
+            hinge.localPosition = p;
+        }
+    }
 }
